61f97f4f5e0a8c85b4ec81de1d8b451ed9a910f0
ferygood <yaochung41@gmail.com>
2024-11-25 18:05:42 +0100

    run prepareRMSK and orthologScale

---
+++ src/hg19_ponAbe2_rmsk.R
@@ -0,0 +1,28 @@
+# this script prepare rmsk for hg19 and ponAbe2, because ponAbe2 rmsk is
+# not in UCSC, so prepareRMSK() fail.
+
+hg19 <- read.table("~/Downloads/hg19.fa.out", header = TRUE,
+                   skip=1, fill=TRUE, stringsAsFactors = FALSE, row.names = NULL)
+ponAbe2 <- read.table("~/Downloads/ponAbe3.fa.out", header = TRUE,
+                      skip=1, fill=TRUE, stringsAsFactors = FALSE, row.names = NULL)
+
+hg19_select <- hg19 %>%
+    select(c(sequence, begin, repeat., class.family))
+colnames(hg19_select)[c(1, 2, 3, 4)] <- c("start", "end", "repName", "repClass")
+
+ponAbe2_select <- ponAbe2 %>%
+    select(c(sequence, begin, repeat., class.family))
+colnames(ponAbe2_select)[c(1, 2, 3, 4)] <- c("start", "end", "repName", "repClass")
+
+hg19_len <- hg19_select %>%
+    mutate(Len = end-start + 1) %>%
+    group_by(repName, repClass) %>%
+    summarise(rLen = mean(Len))
+
+ponAbe2_len <- ponAbe2_select %>%
+    mutate(Len = end-start + 1) %>%
+    group_by(repName, repClass) %>%
+    summarise(cLen = mean(Len))
+
+hm_oran_rmsk <- hg19_len %>%
+    inner_join(ponAbe2_len[,c(1,3)], join_by(repName==repName))
